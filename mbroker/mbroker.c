07ac50507c525afeec5214bc765ed9869a549a99
carlosfelgueiras <catofel2000@gmail.com>
2022-12-26 17:04:28 +0000

    mbroker cli

--- mbroker/mbroker.c
+++ mbroker/mbroker.c
@@ -1,9 +1,28 @@
 #include "logging.h"
+#include <string.h>
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+    char register_pipe[256];
+    int max_sessions = 0;
+    
+    if (argc != 3) {
+        fprintf(stderr, "usage: mbroker <pipename> <max_sessions>\n");
+        exit(-1);
+    }
+
+    if (strlen(argv[1]) > 255) {
+        fprintf(stderr, "usage: mbroker <pipename> <max_sessions>\n");
+        exit(-1);
+    }
+    
+    strcpy(register_pipe, argv[1]);
+    sscanf(argv[2], "%d", &max_sessions);
+
+    if (max_sessions <= 0) {
+        fprintf(stderr, "usage: mbroker <pipename> <max_sessions>\n");
+        exit(-1);
+    }
+
+
+    return 0;
 }
