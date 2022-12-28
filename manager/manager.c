915542bf3586f30f9fdd713ffe6772460055ca19
carlosfelgueiras <catofel2000@gmail.com>
2022-12-28 16:31:00 +0000

    publisher protocol refactor

--- manager/manager.c
+++ manager/manager.c
@@ -13,7 +13,6 @@ int main(int argc, char **argv) {
     char pipe_name[256];
     char box_name[32];
 
-
     if (argc < 4 || argc > 5) {
         print_usage();
         exit(-1);
@@ -28,36 +27,35 @@ int main(int argc, char **argv) {
     strcpy(pipe_name, argv[2]);
 
     switch (argc) {
-        case 4:
-            if (!strcmp(argv[3], "list")) {
-                // TODO: call function that handles list command
-            } else {
-                print_usage();
-                exit(-1);
-            }
-            break;
-        case 5:
-            if (strlen(argv[4]) > 31) {
-                print_usage();
-                exit(-1);
-            }
-
-            if (!strcmp(argv[3], "create")) {
-                strcpy(box_name, argv[4]);
-                // TODO: function that handles create command
-            } else if (!strcmp(argv[3], "remove")) {
-                strcpy(box_name, argv[4]);
-                // TODO: function that handles remove command
-            } else {
-                print_usage();
-                exit(-1);
-            }
-            break;
-        default:
+    case 4:
+        if (!strcmp(argv[3], "list")) {
+            // TODO: call function that handles list command
+        } else {
             print_usage();
             exit(-1);
-    }
+        }
+        break;
+    case 5:
+        if (strlen(argv[4]) > 31) {
+            print_usage();
+            exit(-1);
+        }
 
+        if (!strcmp(argv[3], "create")) {
+            strcpy(box_name, argv[4]);
+            // TODO: function that handles create command
+        } else if (!strcmp(argv[3], "remove")) {
+            strcpy(box_name, argv[4]);
+            // TODO: function that handles remove command
+        } else {
+            print_usage();
+            exit(-1);
+        }
+        break;
+    default:
+        print_usage();
+        exit(-1);
+    }
 
     return -1;
 }
--- publisher/pub.c
+++ publisher/pub.c
@@ -4,6 +4,21 @@
 #define PUBLISHER_REGISTER_CODE_SIZE 289
 #define PUBLISHER_MESSAGE_CODE_SIZE 1025
 
+void register_in_mbroker(char *pipe_name, char *box_name) {
+    char register_code[PUBLISHER_REGISTER_CODE_SIZE] = {0};
+    register_code[0] = 1;
+    memcpy(register_code + 1, pipe_name, 256);
+    memcpy(register_code + 257, box_name, 32);
+    // Send to register pipe
+}
+
+void send_message_to_mbroker(char *message) {
+    char message_code[PUBLISHER_MESSAGE_CODE_SIZE] = {0};
+    message_code[0] = 9;
+    memcpy(message_code + 1, message, 1024);
+    // Send to message pipe
+}
+
 int main(int argc, char **argv) {
     char register_pipe[256];
     char pipe_name[257] = {0};
@@ -14,7 +29,8 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-    if ((strlen(argv[1]) > 256) || (strlen(argv[2]) > 256) || (strlen(argv[3]) > 32)) {
+    if ((strlen(argv[1]) > 256) || (strlen(argv[2]) > 256) ||
+        (strlen(argv[3]) > 32)) {
         fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
         exit(-1);
     }
@@ -23,22 +39,13 @@ int main(int argc, char **argv) {
     strcpy(pipe_name, argv[2]);
     strcpy(box_name, argv[3]);
 
-    char register_code[PUBLISHER_REGISTER_CODE_SIZE] = {0};
-    register_code[0] = 1;
-    memcpy(register_code+1, pipe_name, 256);
-    memcpy(register_code+257, box_name, 32);
-
-    // SEND TO REGISTER PIPE
-
-    char message_code[PUBLISHER_MESSAGE_CODE_SIZE] = {0};
-    register_code[0] = 9;
+    register_in_mbroker(pipe_name, box_name);
 
     char message[1025];
-    while(1) {
+    while (1) {
         memset(message, 0, 1025);
         fgets(message, 1025, stdin);
-        memcpy(message_code+1, message, 1024);
-        // SEND TO MESSAGE PIPE
+        send_message_to_mbroker(message);
     }
 
     return 0;
